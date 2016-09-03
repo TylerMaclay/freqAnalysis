7c66c351533b33eebd70e02371b489c53b75fb2d
Tyler Maclay <tyler@tmaclay.com>
2016-09-03 16:23:17 -0400

    Added wordcount functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 #include "fileReader.h"
+
+std::map<std::string, uint32_t> wordCount(const std::vector<std::string> words);
 
 int main() {
 	const std::string version = "V0.2";
 	std::cout<<"Frequency Analysis Program " << version << std::endl;
 	fileReader file("./test/testfile.txt");
 	std::vector<std::string> testWords = file.getWords();
-	for(auto i : testWords)
-		std::cout<<i<<std::endl;
+	std::map<std::string, uint32_t> testMap = wordCount(testWords);
+	for(auto i : testMap)
+		std::cout<<i.first<<" "<<i.second<<std::endl;
 
 	return 0;
 
+}
+
+std::map<std::string, uint32_t> wordCount(const std::vector<std::string> words){
+	std::map<std::string, uint32_t> wordMap;
+	for(auto i = words.begin(); i != words.end(); i++){
+		auto search = wordMap.find(*i);
+		if(search != wordMap.end())
+			search->second++;
+		else {
+			wordMap.insert(std::make_pair(*i,1));
+		}
+	}
+
+	return wordMap;
 }
